d70fdc72d04983410a9b4f386df1444b9eba2966
Sakaue Aoi <aoiue.w.bbb@gail.com>
2017-02-15 20:55:51 +0900

    Initial Commit

---
+++ Kiroku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kiroku
+//
+//  Created by Aoi Sakaue on 2017/02/15.
+//  Copyright © 2017年 Sakaue Aoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
