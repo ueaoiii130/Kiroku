a7c1eaf8c835d4ecd34e0d97b0611f118e83d9e8
Sakaue Aoi <aoiue.w.bbb@gail.com>
2017-02-23 10:43:34 +0900

    initial commit

---
+++ KirokuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  KirokuViewController.swift
+//  Kiroku
+//
+//  Created by Aoi Sakaue on 2017/02/19.
+//  Copyright © 2017年 Sakaue Aoi. All rights reserved.
+//
+
+import UIKit
+
+class KirokuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MemoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoTableViewCell.swift
+//  Kiroku
+//
+//  Created by Aoi Sakaue on 2017/02/22.
+//  Copyright © 2017年 Sakaue Aoi. All rights reserved.
+//
+
+import UIKit
+
+class MemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
